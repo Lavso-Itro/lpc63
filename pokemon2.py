58267f2a15c12461630afd523aee96558d39ba71
4fterD4rk <110948358+Lavso-Itro@users.noreply.github.com>
2022-09-10 16:01:26 -0500

    agregamos descripción del commit

--- pokemon2.py
+++ pokemon2.py
@@ -21,3 +21,9 @@ def get_pokemons (url='http://pokeapi.co/api/v2/pokemon-form/', offset=0):
 if __name__ == '__main__':
     url = 'http://pokeapi.co/api/v2/pokemon-form/'
     get_pokemons()
+    
+# Script en python que consulta el api de pokemon 
+# para listar los nombres de pokemon pero se le agrego 
+# interacción para que listaras más pokemons segun se vaya requiriendo. 
+# Contribuyo: Brayan Osvaldo Ortiz Mendez
+# Fecha: 10-09-2022
